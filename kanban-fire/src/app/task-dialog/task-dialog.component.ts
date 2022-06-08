1d5a69f5afdf1b9af7dd7671b69a46bb295dfc4d
rmasonh22 <rmasonh@jacquelinetaylorandfriends.org>
2022-06-08 05:36:36 +0000

    create c task-dialog

---
+++ kanban-fire/src/app/task-dialog/task-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-dialog',
+  templateUrl: './task-dialog.component.html',
+  styleUrls: ['./task-dialog.component.css']
+})
+export class TaskDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
